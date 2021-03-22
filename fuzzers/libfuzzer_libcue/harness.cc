5d189605c84645a14b6fdbf7013491a863042804
Maximilian Ammann <max@maxammann.org>
2021-03-22 10:08:34 +0100

    Try to avoid memory leak

--- fuzzers/libfuzzer_libcue/harness.cc
+++ fuzzers/libfuzzer_libcue/harness.cc
@@ -10,16 +10,31 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size == 0) {
     return 0;
   }
+
+  char *test_chars = nullptr;
+  Cd* cd = nullptr;
+  try {
+      test_chars = new char[size + 1];
+      memcpy(test_chars, data, size);
+      test_chars[size - 1] = '\0';
+    
+      Cd* cd;
+      cd = cue_parse_string(test_chars);
+      cd_delete(cd);
+      cd = nullptr;
+
+      delete test_chars;
+  } catch(const std::exception& ex) {
+      if (test_chars != nullptr) {
+        delete test_chars;
+      }
+      if (cd != nullptr) {
+        cd_delete(cd);
+      }
+      throw ex;
+  }
   
-  //freopen("/dev/null", "w", stderr);
 
-  char *test_chars = new char[size + 1];
-  memcpy(test_chars, data, size);
-  test_chars[size - 1] = '\0';
-
-  Cd* cd;
-  cd = cue_parse_string(test_chars);
-  cd_delete(cd);
   return 0;
 }
 
