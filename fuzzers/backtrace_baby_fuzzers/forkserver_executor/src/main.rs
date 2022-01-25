0a740504b53584b2476c1047ff92e7edc97ebabb
Youssef Doubli <doubli@eurecom.fr>
2022-01-25 11:41:53 +0100

    minimal example

--- fuzzers/backtrace_baby_fuzzers/forkserver_executor/src/main.rs
+++ fuzzers/backtrace_baby_fuzzers/forkserver_executor/src/main.rs
@@ -1,4 +1,3 @@
-use core::time::Duration;
 use libafl::{
     bolts::{
         current_nanos,
@@ -6,59 +5,23 @@ use libafl::{
         shmem::{ShMem, ShMemProvider, StdShMemProvider},
         tuples::tuple_list,
     },
-    corpus::{
-        Corpus, InMemoryCorpus, IndexesLenTimeMinimizerCorpusScheduler, OnDiskCorpus,
-        QueueCorpusScheduler,
-    },
+    corpus::{InMemoryCorpus, OnDiskCorpus, QueueCorpusScheduler},
     events::SimpleEventManager,
-    executors::forkserver::{ForkserverExecutor, TimeoutForkserverExecutor},
-    feedback_and_fast, feedback_or,
-    feedbacks::{CrashFeedback, MapFeedbackState, MaxMapFeedback, TimeFeedback},
+    executors::forkserver::ForkserverExecutor,
+    feedbacks::{CrashFeedback, MapFeedbackState, MaxMapFeedback},
     fuzzer::{Fuzzer, StdFuzzer},
+    generators::RandPrintablesGenerator,
     inputs::BytesInput,
     monitors::SimpleMonitor,
     mutators::scheduled::{havoc_mutations, StdScheduledMutator},
-    observers::{ConstMapObserver, HitcountsMapObserver, TimeObserver},
+    observers::{ConstMapObserver, HitcountsMapObserver},
     stages::mutational::StdMutationalStage,
-    state::{HasCorpus, StdState},
+    state::StdState,
 };
 use std::path::PathBuf;
 
-use clap::{App, Arg};
-
 #[allow(clippy::similar_names)]
 pub fn main() {
-    let res = App::new("forkserver_simple")
-        .about("Example Forkserver fuzer")
-        .arg(
-            Arg::new("executable")
-                .help("The instrumented binary we want to fuzz")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::new("in")
-                .help("The directory to read initial inputs from ('seeds')")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::new("timeout")
-                .help("Timeout for each individual execution, in milliseconds")
-                .short('t')
-                .long("timeout")
-                .default_value("1200"),
-        )
-        .arg(
-            Arg::new("arguments")
-                .help("Arguments passed to the target")
-                .setting(clap::ArgSettings::MultipleValues)
-                .takes_value(true),
-        )
-        .get_matches();
-
-    let corpus_dirs = vec![PathBuf::from(res.value_of("in").unwrap().to_string())];
-
     const MAP_SIZE: usize = 65536;
 
     //Coverage map shared between observer and executor
@@ -73,32 +36,16 @@ pub fn main() {
         shmem_map,
     ));
 
-    // Create an observation channel to keep track of the execution time
-    let time_observer = TimeObserver::new("time");
-
     // The state of the edges feedback.
     let feedback_state = MapFeedbackState::with_observer(&edges_observer);
 
-    // The state of the edges feedback for crashes.
-    let objective_state = MapFeedbackState::new("crash_edges", MAP_SIZE);
-
     // Feedback to rate the interestingness of an input
     // This one is composed by two Feedbacks in OR
-    let feedback = feedback_or!(
-        // New maximization map feedback linked to the edges observer and the feedback state
-        MaxMapFeedback::new_tracking(&feedback_state, &edges_observer, true, false),
-        // Time feedback, this one does not need a feedback state
-        TimeFeedback::new_with_observer(&time_observer)
-    );
+    let feedback = MaxMapFeedback::new_tracking(&feedback_state, &edges_observer, true, false);
 
     // A feedback to choose if an input is a solution or not
     // We want to do the same crash deduplication that AFL does
-    let objective = feedback_and_fast!(
-        // Must be a crash
-        CrashFeedback::new(),
-        // Take it onlt if trigger new coverage over crashes
-        MaxMapFeedback::new(&objective_state, &edges_observer)
-    );
+    let objective = CrashFeedback::new();
 
     // create a State from scratch
     let mut state = StdState::new(
@@ -111,7 +58,7 @@ pub fn main() {
         OnDiskCorpus::new(PathBuf::from("./crashes")).unwrap(),
         // States of the feedbacks.
         // They are the data related to the feedbacks that you want to persist in the State.
-        tuple_list!(feedback_state, objective_state),
+        tuple_list!(feedback_state),
     );
 
     // The Monitor trait define how the fuzzer stats are reported to the user
@@ -122,47 +69,27 @@ pub fn main() {
     let mut mgr = SimpleEventManager::new(monitor);
 
     // A minimization+queue policy to get testcasess from the corpus
-    let scheduler = IndexesLenTimeMinimizerCorpusScheduler::new(QueueCorpusScheduler::new());
+    let scheduler = QueueCorpusScheduler::new();
 
     // A fuzzer with feedbacks and a corpus scheduler
     let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
 
     // Create the executor for the forkserver
-    let args = match res.values_of("arguments") {
-        Some(vec) => vec.map(|s| s.to_string()).collect::<Vec<String>>().to_vec(),
-        None => [].to_vec(),
-    };
-
-    let mut executor = TimeoutForkserverExecutor::new(
-        ForkserverExecutor::new(
-            res.value_of("executable").unwrap().to_string(),
-            &args,
-            true,
-            tuple_list!(edges_observer, time_observer),
-        )
-        .unwrap(),
-        Duration::from_millis(
-            res.value_of("timeout")
-                .unwrap()
-                .to_string()
-                .parse()
-                .expect("Could not parse timeout in milliseconds"),
-        ),
+    let mut executor = ForkserverExecutor::new(
+        "./target/release/program".to_string(),
+        &[],
+        true,
+        tuple_list!(edges_observer),
     )
     .expect("Failed to create the executor.");
 
-    // In case the corpus is empty (on first run), reset
-    if state.corpus().count() < 1 {
-        state
-            .load_initial_inputs(&mut fuzzer, &mut executor, &mut mgr, &corpus_dirs)
-            .unwrap_or_else(|err| {
-                panic!(
-                    "Failed to load initial corpus at {:?}: {:?}",
-                    &corpus_dirs, err
-                )
-            });
-        println!("We imported {} inputs from disk.", state.corpus().count());
-    }
+    // Generator of printable bytearrays of max size 32
+    let mut generator = RandPrintablesGenerator::new(3);
+
+    // Generate 8 initial inputs
+    state
+        .generate_initial_inputs(&mut fuzzer, &mut executor, &mut generator, &mut mgr, 8)
+        .expect("Failed to generate the initial corpus");
 
     // Setup a mutational stage with a basic bytes mutator
     let mutator = StdScheduledMutator::new(havoc_mutations());
